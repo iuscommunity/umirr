ace3d9db351f00b28fca04781921e602706f3b7b
cgtx <carl@cgtx.us>
2015-03-04 14:46:32 -0600

    make routes customizable

--- umirr/__init__.py
+++ umirr/__init__.py
@@ -5,16 +5,17 @@ from .resources import MainResource, MirrorsResource, MirrorListResource
 
 application = falcon.API()
 
-# home page
+# main page
+main_settings = settings.get('main')
 main_resource = MainResource()
-application.add_route('/', main_resource)
+application.add_route(main_settings.get('route'), main_resource)
 
 # expose mirror data
 mirrors_settings = settings.get('mirrors')
 mirrors_resource = MirrorsResource(mirrors_settings, mirrors)
-application.add_route('/mirrors', mirrors_resource)
+application.add_route(mirrors_settings.get('route'), mirrors_resource)
 
 # generate the list of mirrors
 mirrorlist_settings = settings.get('mirrorlist')
 mirrorlist_resource = MirrorListResource(mirrorlist_settings, mirrors)
-application.add_route('/mirrorlist', mirrorlist_resource)
+application.add_route(mirrorlist_settings.get('route'), mirrorlist_resource)
