b0e5d6c23f485efe610652fd5d6216cd0e181749
cgtx <carl@cgtx.us>
2015-03-04 15:05:59 -0600

    add option to enable/disable each resource

--- umirr/__init__.py
+++ umirr/__init__.py
@@ -7,15 +7,18 @@ application = falcon.API()
 
 # main page
 main_settings = settings.get('main')
-main_resource = MainResource()
-application.add_route(main_settings.get('route'), main_resource)
+if main_settings.get('enabled'):
+    main_resource = MainResource()
+    application.add_route(main_settings.get('route'), main_resource)
 
 # expose mirror data
 mirrors_settings = settings.get('mirrors')
-mirrors_resource = MirrorsResource(mirrors_settings, mirrors)
-application.add_route(mirrors_settings.get('route'), mirrors_resource)
+if mirrors_settings.get('enabled'):
+    mirrors_resource = MirrorsResource(mirrors_settings, mirrors)
+    application.add_route(mirrors_settings.get('route'), mirrors_resource)
 
 # generate the list of mirrors
 mirrorlist_settings = settings.get('mirrorlist')
-mirrorlist_resource = MirrorListResource(mirrorlist_settings, mirrors)
-application.add_route(mirrorlist_settings.get('route'), mirrorlist_resource)
+if mirrorlist_settings.get('enabled'):
+    mirrorlist_resource = MirrorListResource(mirrorlist_settings, mirrors)
+    application.add_route(mirrorlist_settings.get('route'), mirrorlist_resource)
