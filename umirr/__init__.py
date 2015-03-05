4629fdb580785c0b78474d91caa901f8c01a67a1
cgtx <carl@cgtx.us>
2015-03-05 12:29:47 -0600

    rearrange this slightly

--- umirr/__init__.py
+++ umirr/__init__.py
@@ -8,17 +8,20 @@ application = falcon.API()
 # main page
 main_settings = settings.get('main')
 if main_settings.get('enabled'):
+    main_route = main_settings.get('route')
     main_resource = MainResource()
-    application.add_route(main_settings.get('route'), main_resource)
+    application.add_route(main_route, main_resource)
 
 # expose mirror data
 mirrors_settings = settings.get('mirrors')
 if mirrors_settings.get('enabled'):
+    mirrors_route = mirrors_settings.get('route')
     mirrors_resource = MirrorsResource(mirrors_settings, mirrors)
-    application.add_route(mirrors_settings.get('route'), mirrors_resource)
+    application.add_route(mirrors_route, mirrors_resource)
 
 # generate the list of mirrors
 mirrorlist_settings = settings.get('mirrorlist')
 if mirrorlist_settings.get('enabled'):
+    mirrorlist_route = mirrorlist_settings.get('route')
     mirrorlist_resource = MirrorListResource(mirrorlist_settings, mirrors)
-    application.add_route(mirrorlist_settings.get('route'), mirrorlist_resource)
+    application.add_route(mirrorlist_route, mirrorlist_resource)
